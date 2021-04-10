7aec52345b948f81fd49040b1540b6dbc1614df2
HillaryJR <hillaryjeramin@gmail.com>
2021-04-10 16:18:04 +0530

    First commit

---
+++ ProductService/src/main/java/com/sliit/product/Main.java
@@ -0,0 +1,13 @@
+package com.sliit.product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hellow Product Service");
+		
+
+	}
+
+}
